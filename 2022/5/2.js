2526a80ea11c498d9376b19b4fa1457adcabc686
Simon Lorenz <mail@simon-lorenz.dev>
2022-12-25 21:25:19 +0100

    add 5/2

---
+++ 2022/5/2.js
@@ -0,0 +1,68 @@
+import fs from 'fs'
+import { EOL } from 'os'
+import path from 'path'
+
+import { __dirname } from '#root/util/index.js'
+
+const input = fs.readFileSync(path.join(__dirname(import.meta), './input.txt'), 'utf-8')
+const lines = input.split(EOL)
+
+const divider = lines.indexOf('')
+
+const initialStateMatrix = []
+const moves = []
+
+lines.forEach((line, index) => {
+	// Ignore divider & final newline
+	if (!line.length > 0) {
+		return
+	}
+
+	if (index < divider) {
+		initialStateMatrix.push(line)
+	} else {
+		moves.push(line)
+	}
+})
+
+const numbers = initialStateMatrix[initialStateMatrix.length - 1]
+
+const stacks = {}
+
+for (let index = 0; index < numbers.length; index++) {
+	const char = numbers[index]
+
+	if (char !== ' ') {
+		stacks[char] = index
+	}
+}
+
+for (const [a, b] of Object.entries(stacks)) {
+	stacks[a] = []
+	initialStateMatrix.forEach(line => {
+		if (line[b] !== ' ') {
+			stacks[a].unshift(line[b])
+		}
+	})
+}
+
+for (const move of moves) {
+	// eslint-disable-next-line prefer-const
+	let [[count], [source], [dest]] = move.matchAll(/(\d+)/g)
+
+	const stackOfCrates = []
+
+	while (count > 0) {
+		stackOfCrates.unshift(stacks[source].pop())
+		count--
+	}
+
+	stacks[dest].push(...stackOfCrates)
+}
+
+const solution = Object.entries(stacks).reduce((solution, [_, stack]) => {
+	console.log(`Last element of stack ${_}: ${stack[stack.length - 1]}`)
+	return solution + stack[stack.length - 1]
+}, '')
+
+console.log(`Solution: ${solution}`)
