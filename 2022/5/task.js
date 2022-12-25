5eaa592f8a5a1860e40c971fcd6f386c21784691
Simon Lorenz <mail@simon-lorenz.dev>
2022-12-25 20:03:06 +0100

    refactor: fix code style, add util, handle os specific eol

--- 2022/5/task.js
+++ 2022/5/task.js
@@ -1,54 +1,64 @@
 import fs from 'fs'
+import { EOL } from 'os'
+import path from 'path'
 
-const input = fs.readFileSync('./input', 'utf-8')
-const lines = input.split('\r\n')
+import { __dirname } from '#root/util/index.js'
+
+const input = fs.readFileSync(path.join(__dirname(import.meta), './input.txt'), 'utf-8')
+const lines = input.split(EOL)
 
 const divider = lines.indexOf('')
 
-let matrix = []
+const matrix = []
 const moves = []
 
 for (let line = 0; line < lines.length - 1; line++) {
-    if (line === divider) 
-        continue
+	if (line === divider) {
+		continue
+	}
 
-    if (line < divider) {
-        matrix.push(lines[line])
-    } else {
-        moves.push(lines[line])
-    }
+	if (line < divider) {
+		matrix.push(lines[line])
+	} else {
+		moves.push(lines[line])
+	}
 }
 
 const numbers = matrix[matrix.length - 1]
+
 const stacks = {}
 
 for (let index = 0; index < numbers.length; index++) {
-    const char = numbers[index];
+	const char = numbers[index]
 
-    if (char !== ' ') {
-        stacks[char] = index
-    }
+	if (char !== ' ') {
+		stacks[char] = index
+	}
 }
 
 for (const [a, b] of Object.entries(stacks)) {
-    stacks[a] = []
-    matrix.forEach(line => {
-        if (line[b] !== ' ') {
-            stacks[a].unshift(line[b])
-        }
-    })
+	stacks[a] = []
+	matrix.forEach(line => {
+		if (line[b] !== ' ') {
+			stacks[a].unshift(line[b])
+		}
+	})
 }
 
 for (const line of lines) {
-    let [count, source, dest] = line.replace('move ', '').replace('from ', '').replace('to ', '').split(' ')
+	// eslint-disable-next-line prefer-const
+	let [count, source, dest] = line.replace('move ', '').replace('from ', '').replace('to ', '').split(' ')
 
-    while(count > 0) {
-        const element = stacks[source].pop();
-        stacks[dest].push(element)
-        count--
-    }
+	while (count > 0) {
+		const element = stacks[source].pop()
+		stacks[dest].push(element)
+		count--
+	}
 }
 
-for (const [stack, elements] of Object.entries(stacks)) {
-    console.log('Last element of stack ' + stack  + ': ' + elements[elements.length - 1])
-}
+const solution = Object.entries(stacks).reduce((solution, [_, stack]) => {
+	console.log(`Last element of stack ${_}: ${stack[stack.length - 1]}`)
+	return solution + stack[stack.length - 1]
+}, '')
+
+console.log(`Solution: ${solution}`)
