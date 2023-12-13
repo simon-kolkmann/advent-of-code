04603ff5d3bc058eb0e28ad05dcfb444169bbe60
Simon Lorenz <mail@simon-lorenz.dev>
2023-12-13 20:12:06 +0100

    2023: task 2

--- 2023/tasks/2/2.go
+++ 2023/tasks/2/2.go
@@ -1,15 +1,84 @@
 package task_2
 
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	"simon-lorenz.dev/advent-of-code/2023/utils"
+)
+
 type Solver struct{}
 
 func (Solver) GetPuzzleNumber() int {
 	return 2
 }
 
-func (Solver) SolveFirst() string {
-	return "N/A"
+func (solver Solver) GetPuzzleInput() ([]string, error) {
+	return utils.GetPuzzleInput(solver.GetPuzzleNumber())
 }
 
-func (Solver) SolveSecond() string {
-	return "N/A"
+func (solver Solver) SolveFirst() string {
+	lines, _ := solver.GetPuzzleInput()
+
+	solution := 0
+
+	for _, line := range lines {
+		var stats map[string]int = make(map[string]int)
+
+		splitted := strings.Split(line, ": ")
+		game := splitted[0]
+		rounds := splitted[1]
+
+		number, _ := strconv.Atoi(regexp.MustCompile(`\d+`).FindStringSubmatch(game)[0])
+
+		for _, round := range strings.Split(rounds, "; ") {
+			for _, cubesOfColor := range strings.Split(round, ", ") {
+				countAndColor := strings.Split(cubesOfColor, " ")
+				count, color := countAndColor[0], countAndColor[1]
+
+				countAsInt, _ := strconv.Atoi(count)
+
+				if stats[color] < countAsInt {
+					stats[color] = countAsInt
+				}
+			}
+		}
+
+		if stats["red"] <= 12 && stats["green"] <= 13 && stats["blue"] <= 14 {
+			solution += number
+		}
+	}
+
+	return strconv.Itoa(solution)
+}
+
+func (solver Solver) SolveSecond() string {
+	lines, _ := solver.GetPuzzleInput()
+
+	solution := 0
+
+	for _, line := range lines {
+		var stats map[string]int = make(map[string]int)
+
+		splitted := strings.Split(line, ": ")
+		rounds := splitted[1]
+
+		for _, round := range strings.Split(rounds, "; ") {
+			for _, cubesOfColor := range strings.Split(round, ", ") {
+				countAndColor := strings.Split(cubesOfColor, " ")
+				count, color := countAndColor[0], countAndColor[1]
+
+				countAsInt, _ := strconv.Atoi(count)
+
+				if stats[color] < countAsInt {
+					stats[color] = countAsInt
+				}
+			}
+		}
+
+		solution += stats["red"] * stats["green"] * stats["blue"]
+	}
+
+	return strconv.Itoa(solution)
 }
