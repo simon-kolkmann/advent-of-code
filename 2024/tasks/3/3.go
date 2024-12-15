233700202c40f26710869aa5dcf512531dce9229
Simon Lorenz <mail@simon-lorenz.dev>
2024-12-15 22:51:45 +0100

    solve 2024/3/2

--- 2024/tasks/3/3.go
+++ 2024/tasks/3/3.go
@@ -3,6 +3,7 @@ package task_3
 import (
 	"regexp"
 	"strconv"
+	"strings"
 
 	"simon-lorenz.dev/advent-of-code/2024/utils"
 )
@@ -19,20 +20,51 @@ func NewSolver() Solver {
 
 func (solver Solver) SolveFirst() string {
 	lines, _ := solver.GetPuzzleInput()
+	line := strings.Join(lines, "")
 	sum := 0
 
-	for _, line := range lines {
-		operations := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`).FindAllString(line, -1)
+	operations := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`).FindAllString(line, -1)
 
-		for _, operation := range operations {
-			numbers := regexp.MustCompile(`\d{1,3}`).FindAllString(operation, 2)
-			sum += utils.UnsafeAtoi(numbers[0]) * utils.UnsafeAtoi(numbers[1])
-		}
+	for _, operation := range operations {
+		numbers := regexp.MustCompile(`\d{1,3}`).FindAllString(operation, 2)
+		sum += utils.UnsafeAtoi(numbers[0]) * utils.UnsafeAtoi(numbers[1])
 	}
 
 	return strconv.Itoa(sum)
 }
 
 func (solver Solver) SolveSecond() string {
-	return ""
+	lines, _ := solver.GetPuzzleInput()
+	line := strings.Join(lines, "")
+	sum := 0
+
+	code := ""
+	enabled := true
+
+	for i, char := range line {
+		if enabled {
+			code += string(char)
+		}
+
+		if i > len(line)-7 {
+			continue
+		}
+
+		if enabled && line[i:i+7] == "don't()" {
+			enabled = false
+		}
+
+		if !enabled && line[i:i+4] == "do()" {
+			enabled = true
+		}
+	}
+
+	operations := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`).FindAllString(code, -1)
+
+	for _, operation := range operations {
+		numbers := regexp.MustCompile(`\d{1,3}`).FindAllString(operation, 2)
+		sum += utils.UnsafeAtoi(numbers[0]) * utils.UnsafeAtoi(numbers[1])
+	}
+
+	return strconv.Itoa(sum)
 }
